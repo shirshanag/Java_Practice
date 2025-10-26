05e298221dfcfd03e23d235f31315052c3c06c24
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 00:48:58 +0530

    Create char.java

---
+++ String/Special-characters/char.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[]){
+      //Add double quotations in string
+    String txt="we are so called \"vikings\" from the north ";
+      //Add single quotations in string
+    String txt1="we are so called \'vikings\' from the north ";
+      //Add newline character
+    String txt2="we are so called \n vikings from the north ";
+    System.out.println(txt);
+    System.out.println(txt1);
+    System.out.println(txt2);
+    }
+}
