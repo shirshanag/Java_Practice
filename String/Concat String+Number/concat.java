9a973536d0556705fa9127e97511a662fc0f2dd7
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-26 22:14:42 +0530

    Create concat.java

---
+++ String/Concat String+Number/concat.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String args[]){
+      //Take user input
+        String x="10";
+        String y="20";
+        String a="30";
+        int b=40;
+        String w="10";
+        int e=20;  
+        String z= w+e;
+      //ADD two nos
+        System.out.println("Concat as number:"+(a+b));
+      //Concat two strings
+        System.out.println("Concat as string:"+x+y);
+      //concat one string and one number
+        System.out.println("Concat string+number:"+z);
+    }
+}
