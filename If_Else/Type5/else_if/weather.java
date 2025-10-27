b5279bc88f58fa1b2a45e2d2a82739ff90f597ec
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 21:34:49 +0530

    Create weather.java

---
+++ If_Else/Type5/else_if/weather.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String args[]){
+        int weather=1;
+        if(weather==1){
+            System.out.println("Take an umbrella outside!!");
+    }
+        else if(weather==2){
+            System.out.println("It's Rainning!");
+        }
+        else{
+            System.out.println("Its normal outside!!");
+        }
+    }
+}
