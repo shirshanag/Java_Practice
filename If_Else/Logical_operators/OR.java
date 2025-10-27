a993016202f75404bad5f058e5edaa97857ca7ae
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 23:51:50 +0530

    Create OR.java

---
+++ If_Else/Logical_operators/OR.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]){
+        int a=22;
+        int b=23;
+        int c=50;
+        if(a>b||c>b){
+            System.out.println("Atleast one  condition is true!!");
+        }
+    }
+}
