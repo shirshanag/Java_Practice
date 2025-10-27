38876ebba4a1ae57bf46d706c0ec52928960f85c
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 19:38:09 +0530

    Create bool.java

---
+++ If_Else/Type3/bool.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]){
+       boolean isRaining=true;
+      //Checking boolean value
+       if(isRaining){
+           System.out.println("It's raining take out an umbrella!!");
+       }
+    
+    }
+}
