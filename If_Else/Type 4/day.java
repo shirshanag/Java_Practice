f0bec9acdcadabff98e180272f461fddb2488e0f
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 21:23:58 +0530

    Create day.java

---
+++ If_Else/Type 4/day.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String args[]){
+        int day=16;
+        if(day<18){
+            System.out.println("Good Day!!");
+        }
+        else{
+            System.out.println("Good Evening!!");
+        }
+    }
+}
