2bae4e86a96c64549c17a4b2f0af0342d04f0e9b
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 19:29:26 +0530

    Create equal_not_equal.java

---
+++ If_Else/Type2/equal_not_equal.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[]){
+        int a=18;
+        int b=23;
+        int c=18;
+        if(a==c){
+            System.out.println("No. is equal!!");
+        }
+        if(a!=b){
+            System.out.println("true");
+        }
+    }
+}
