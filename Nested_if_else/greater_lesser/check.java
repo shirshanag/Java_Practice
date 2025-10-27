236672ae2cd568a923b6c69ae8c1d22bb8bf8aa7
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 22:46:30 +0530

    Create check.java

---
+++ Nested_if_else/greater_lesser/check.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main (String args[]){
+        int x=10;
+        int y=20;
+        if(x>6){
+            System.out.println("Greater!!");
+        }
+        if(y<21){
+            System.out.println("Lesser!!");
+        }
+    }
+}
