6ef0d70421135a8dd6459295b84ef277a6f04bb9
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 23:55:31 +0530

    Create cond.java

---
+++ If_Else/Logical_operators/AND/cond.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]){
+        int a=22;
+        int b=23;
+        int c=50;
+        if(b>a&&c>b){
+            System.out.println("Both condition is true!!");
+        }
+    }
+}
