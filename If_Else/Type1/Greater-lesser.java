fc2b513040a55864ee3baa18610a877c32c60e6b
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 19:26:52 +0530

    Create Greater-lesser.java

---
+++ If_Else/Type1/Greater-lesser.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String args[]){
+        int a=18;
+        int b=23;
+        int c=18;
+        if(a>b){
+            System.out.println("No is  Greater!!");
+        }
+        else{
+            System.out.println("No. is lesser!!");
+        }
+    }
+}
