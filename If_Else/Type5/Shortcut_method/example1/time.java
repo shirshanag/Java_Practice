e36159d8468945ba3a7fd1ddbb47a17f616e2436
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 22:01:48 +0530

    Create time.java

---
+++ If_Else/Type5/Shortcut_method/example1/time.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String args[]){
+        int time=10;
+        String text=(time<12)?"Good Morning!!":"Good Evening!!";
+        System.out.println(text);
+    }
+}
