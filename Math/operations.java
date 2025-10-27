f1b3ca9a4555c4d87460ac243d7ff0437e9982f8
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-27 18:20:59 +0530

    Create operations.java

---
+++ Math/operations.java
@@ -0,0 +1,22 @@
+class Main{
+    public static void main(String args[]){
+      //Find maximum between two numbers
+       System.out.println("The max between 10 and 5:"+ Math.max(10,5));
+      //Find minimum between two numbers
+       System.out.println("The minimum between 10 and 5:"+Math.min(10,5));
+      //Find Square root of a given number
+       System.out.println("Returns square root of 64:"+Math.sqrt(64));
+      //Find the absolute positive of a given negetive number
+       System.out.println("Absolute positive value of -4.5:"+Math.abs(-4.5));
+      //Find the value of power of certain numbers
+       System.out.println("2 raise to the power 8:"+Math.pow(2,8));
+      //Rounding off certain no to its nearest values
+       System.out.println("Round to nearest integer:"+Math.round(4.1));
+      //Rounding off certain no to its ceil value
+       System.out.println("Round to ceil value:"+Math.ceil(4.1));
+      //Rounding off certain no to its root value
+       System.out.println("Round to floor value:"+Math.floor(4.9));
+      //Generate a random number between 0(inclusive) and 1(exclusive)
+       System.out.println("Generate a random number between 0.0 (inclusive) to 1.0(exclusive):"+Math.random());
+    }
+}
