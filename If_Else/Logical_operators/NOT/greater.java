41bfc9307de05594e76b9b44039925e9d6fa21a4
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-29 21:04:04 +0530

    Add greater.java to demonstrate NOT operator

---
+++ If_Else/Logical_operators/NOT/greater.java
@@ -0,0 +1,11 @@
+
+
+class Main{
+    public static void main(String args[]){
+        int a=33;
+        int b=55;
+        if(!(a>b)){
+            System.out.println("a is not greater than b");
+        }
+    }
+}
