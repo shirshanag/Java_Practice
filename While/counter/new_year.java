1030836adfbbbc09bd1eaf81818e21026fa75adb
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-29 23:32:39 +0530

    Add countdown program for New Year celebration

---
+++ While/counter/new_year.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String args[]){
+        int countdown=3;
+        while(countdown>0){
+            System.out.println(countdown);
+            countdown --;
+        }
+        System.out.println("Happy new year!!");
+    }
+}
