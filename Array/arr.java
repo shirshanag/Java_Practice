cc42175ac63fa60de50db5290801ba909439c589
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-10-31 00:12:10 +0530

    Create arr.java

---
+++ Array/arr.java
@@ -0,0 +1,25 @@
+class Main{
+    public static void main(String args[]){
+        //Accessing Zeroth element of the array
+        System.out.println("===Accessing Zeroth element of the array===");
+        String [] arr={"Audi","Skoda","Wagonar"};
+        System.out.println(arr[0]);
+        //Change the Zeroth element of the array.
+        System.out.println("===Change the Zeroth element of the array.===");
+        arr[0]="BMW";
+        for(String n:arr){
+            System.out.println(n);
+        }
+        System.out.println("===Length of the array===");
+        System.out.println(arr.length);
+        System.out.println("===Creating Array with new keyword===");
+        String [] car=new String[4];
+        car[0]="Audi";
+        car[1]="BMW";
+        car[2]="Waganor";
+        car[3]="Swift";
+        System.out.println(car[0]);
+    
+}
+         
+    }
