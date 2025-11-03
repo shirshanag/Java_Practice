909e78ca728f38e0c66e8aa0d3f6d99cd24b2188
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-03 21:33:16 +0530

    Create add.java

---
+++ Method/Return_value/addition/add.java
@@ -0,0 +1,8 @@
+public class Main{
+    static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String args[]){
+        System.out.println("Add:"+add(10,20));
+    }
+}
