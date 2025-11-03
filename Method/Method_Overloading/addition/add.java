2474dd5eedc29540797b88eb06eecb7bac1ff445
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-03 23:38:36 +0530

    Create add.java

---
+++ Method/Method_Overloading/addition/add.java
@@ -0,0 +1,12 @@
+public class Main{
+    static int intadd(int a, int b){
+        return a+b;
+    }
+    static double doubleadd(double a, double b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        System.out.println("Double addition:"+doubleadd(2.3,4.5));
+        System.out.println("Integer addition:"+intadd(2,4));
+    }
+}
