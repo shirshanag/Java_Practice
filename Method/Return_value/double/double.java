e469a3ef83a0a4140b8fa11de0118be2b9447a5b
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-03 21:42:46 +0530

    Update double.java

--- Method/Return_value/double/double.java
+++ Method/Return_value/double/double.java
@@ -3,6 +3,9 @@ public class Main{
         return x*2;
     }
     public static void main(String [] args){
-        System.out.println(doubleGame(3));
+        for(int i=0;i<=5;i++){
+            System.out.println("Double of "+ i +" is: "+doubleGame(i));
+        }
+        
     }
 }
