9a2eeb8b686e80b3f69db696f5255d415b4eb5c8
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-03 20:37:49 +0530

    Create name.java

---
+++ Method/Multiparametres/name.java
@@ -0,0 +1,8 @@
+public class Main{
+    static void myMethod(String fname,int age){
+        System.out.println(fname + " is " +age);
+    }
+    public static void main(String[] args){
+        myMethod("Liam",20);
+    }
+}
