0444c63836a26796deef0339c829ca7b1b783456
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-04 11:36:42 +0530

    Create obj2.java

---
+++ Classes_objects/Multiple_object/obj2.java
@@ -0,0 +1,10 @@
+public class Main{
+    int x=10;
+    int y=12;
+    public static void main(String args[]){
+        Main obj1=new Main();
+        Main obj2=new Main();
+        System.out.println("value of object 1:"+obj1.x);
+        System.out.println("Value of object 2:"+obj2.y);
+    }
+}
