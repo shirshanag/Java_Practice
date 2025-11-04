224b5fc763c454e35fbffccc0924f0bc1351a0df
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-05 00:20:46 +0530

    Create Main.java

---
+++ this_keyword/Accessing_class_parametres/Main.java
@@ -0,0 +1,11 @@
+
+class Main{
+    int x;
+    public Main(int x){
+        this.x=x;
+    }
+    public static void main(String[] args){
+        Main myobj=new Main(56);
+        System.out.println(myobj.x);
+    }
+}
