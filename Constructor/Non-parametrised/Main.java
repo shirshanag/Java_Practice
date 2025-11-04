8341fe801820f910e26a6410d037bf282a7d4c37
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-04 23:40:08 +0530

    Update Main.java

--- Constructor/Non-parametrised/Main.java
+++ Constructor/Non-parametrised/Main.java
@@ -3,7 +3,7 @@ class Main{
     public Main(){
         x=20;
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         Main obj1=new Main();
         System.out.println(obj1.x);
     } 
