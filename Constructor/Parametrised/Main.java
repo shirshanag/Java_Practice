458809385c9e65087ceba941f92e8314bfe813ed
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-04 22:04:49 +0530

    Create Main.java

---
+++ Constructor/Parametrised/Main.java
@@ -0,0 +1,11 @@
+
+class Main{
+    int x;
+    public Main(int y){
+        x=y;
+    }
+    public static void main(String args[]){
+        Main obj1=new Main(5);
+        System.out.println(obj1.x);
+    } 
+}
