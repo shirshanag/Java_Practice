f3ae9286cdeebdcd4bd21e3846b21a59dc193956
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-04 19:27:10 +0530

    Create Main.java

---
+++ Class_Attributes/Double_Attributes/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    String fname="Rohan";
+    String lname="Mehta";
+    int age=20;
+
+    public static void main(String[] args){
+        Main obj1=new Main();
+        System.out.println("Name: "+obj1.fname+" "+obj1.lname);
+        System.out.println("Age:"+obj1.age);
+    }
+}
