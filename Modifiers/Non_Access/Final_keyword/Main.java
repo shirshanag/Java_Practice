5ba51ee08b6e64c27aafe2b101412b9221e1e4a9
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-05 11:02:29 +0530

    Create Main.java

---
+++ Modifiers/Non_Access/Final_keyword/Main.java
@@ -0,0 +1,9 @@
+class Math{
+    final double pi=3.14;
+    final int n=10;
+    public static void main(String[] args){
+        Math obj1=new Math();
+        obj1.pi=2.3;
+        System.out.println(obj1.pi);
+    }
+}
