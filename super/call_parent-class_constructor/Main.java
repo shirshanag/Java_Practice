cfc60b0b1c5bcf6288a119b9ac501450a3fbd9c0
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-05 22:40:30 +0530

    Create Main.java

---
+++ super/call_parent-class_constructor/Main.java
@@ -0,0 +1,16 @@
+class Main{
+    public Main(){
+        System.out.println("Class Cinstructor is created!!");
+    }
+}
+class Name extends Main{
+    public Name(){
+        super();
+        System.out.println("Subclass Constructor is created!!");
+    }
+}
+class Demo{
+public static void main(String[] args){
+    Name name=new Name();
+}
+}
