aa4712c9a7d049a54e820cd3608050a1e04f31b3
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-05 11:06:30 +0530

    Create Main.java

---
+++ Modifiers/Non_Access/Static_keyword/Main.java
@@ -0,0 +1,9 @@
+class Math{
+    static void myMethod(){
+        System.out.println("My Static method");
+    }
+    //No need of creating object as its a static method
+    public static void main(String[] args){
+        myMethod();
+    }
+}
