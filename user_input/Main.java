68319be73012ef582de7631047ec460c56961e3b
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-06 20:44:15 +0530

    Create Main.java

---
+++ user_input/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String [] args){
+        Scanner myobj=new Scanner(System.in);
+       
+        String username=myobj.nextLine();
+        System.out.println("User name:"+username);
+        }
+}
