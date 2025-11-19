9005598f7225c4771fca13fcb7dafe1f04a5330f
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-11-19 12:04:40 +0530

    Create Main.java

---
+++ Stack/Main.java
@@ -0,0 +1,36 @@
+interface StackADT{
+    void  push(int element);
+    int pop();
+    int peek();
+    boolean isEmpty();
+    }
+class Stack implements StackADT{
+    private int[] stack;
+    private int top;
+    private final int cap=100;
+    public Stack(){
+        stack=new int[cap];
+        top=-1;
+    }
+    public void push(int element){
+        stack[++top]=element;
+    }
+    public int pop(){
+        return stack[--top];
+    }
+    public int peek(){
+        return stack[top];
+    }
+    public boolean isEmpty(){
+        return top==-1;
+    }
+}
+class Main{
+    public static void main(String[] args){
+        StackADT stack=new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.pop());
+    }
+}
