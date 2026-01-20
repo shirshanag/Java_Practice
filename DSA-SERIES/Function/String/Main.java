1a44cbf9e4b2e8426b9061d85da195ba17f7094a
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2026-01-20 20:09:47 +0530

    Create Main.java

---
+++ DSA-SERIES/Function/String/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        //
+        String g=greet();
+        System.out.print(g);
+    }
+    static String greet(){
+        //We want to return the string when the function/method is called;
+        return "Who are you?";
+    }
+}
