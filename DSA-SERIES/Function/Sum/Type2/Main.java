bd0069d4f0d1c98fb7dbe1310b0cf66d89fb0c1c
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2026-01-20 20:22:03 +0530

    Add Main class with argument-based addition method

---
+++ DSA-SERIES/Function/Sum/Type2/Main.java
@@ -0,0 +1,12 @@
+//Argument based
+class Main{
+    public static void main(String[] args){
+        //So the value is passed here
+        int a=add(10,30);
+        System.out.println(a);
+            }
+    static int add(int a,int b){
+        //Here we will create an  arguement where values will be called while calling the method
+        return a+b;
+    }
+    }
