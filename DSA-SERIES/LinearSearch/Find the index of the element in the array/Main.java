f8f761e902dd4dba600b89c1989df7cbf530afda
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2026-05-06 23:43:21 +0530

    Create Main.java

---
+++ DSA-SERIES/LinearSearch/Find the index of the element in the array/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] arr={12,3,2,4,5};
+		int num=4; 
+		for(int i=0;i<arr.length;i++){
+		    if(arr[i]==num){
+		        System.out.println(i);
+		    }
+		}
+
+	}
+}
